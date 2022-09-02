093a6f10111e64144d4d455a9b2e6593611c8df0
Alexis GUYON <alexis103@upschool.fr>
2022-09-02 18:50:05 +0200

    NestJS Personnal Project

---
+++ README.md
@@ -0,0 +1,88 @@
+<h2 align="center">
+  Personnal Project to learn <a href="http://nestjs.com/" target="blank">NestJS</a>
+</h2>
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="150" alt="Nest Logo" /></a>
+</p>
+
+# What I learned ?
+
+### NestJS
+- Modules
+- Controllers
+- Services and Providers
+- Controller-to-Service communication
+- Validation using Pipes
+
+### Back-end & Architecture
+- Production-ready REST APIs
+- CRUD Operations
+- Error Handling
+- Data Transfer Objects (DTO)
+- System modularity
+- Back-end development best practices
+- Configuration Management
+- Logging
+- Security best practices
+- Unit testing
+
+### Persistence
+- Connecting the application to a database
+- Working with relational databases
+- Using TypeORM
+- Writing simple and complex queries using QueryBuilder
+- Performance
+
+### Authorization / Authentication
+- Signing up, signing in
+- Authentication and Authorization
+- Protected resources
+- Ownership of tasks by users
+- Using JWT tokens (JSON Web Token)
+- Password hashing, salts and properly storing passwords
+
+### Deployment
+- Polishing the application for Production use
+- Deploying NestJS apps to AWS
+- Deploying front-end apps to Amazon S3
+- Wiring up the front-end and back-end
+
+### Front-end application
+- Fully-featured front-end application that consumes the API
+
+<br />
+
+# Documentation
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
+
---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,64 @@
+import { NotFoundException } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { TaskStatus } from './task-status.enum';
+import { TasksRepository } from './tasks.repository';
+import { TasksService } from './tasks.service';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockUser = {
+  id: 'someId',
+  username: 'Alex',
+  password: 'somePassword',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepository = module.get(TasksRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls tasksRepository.getTasks and returns the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls tasksRepository.findOne and returns the result', async () => {
+      const mockTask = {
+        id: 'someId',
+        title: 'someTitle',
+        description: 'someDescription',
+        status: TaskStatus.OPEN,
+      };
+
+      await tasksRepository.findOne.mockResolvedValue(mockTask);
+      const result = await tasksService.getTaskById('someId', mockUser);
+      expect(result).toEqual(mockTask);
+    });
+
+    it('calls tasksRepository.findOne and handles an error', async () => {
+      tasksRepository.findOne.mockResolvedValue(null);
+      expect(tasksService.getTaskById('someId', mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+});
